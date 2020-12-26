7fdcd2341d472a1f9baf43127b5676dea009e6a4
Marshall G Gates <mggates@gmail.com>
2020-12-25 22:29:11 -0500

    Add the real temperature control logic

--- Cat_House.ino
+++ Cat_House.ino
@@ -18,9 +18,9 @@ products from Adafruit!
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 #define RELAY_PIN 8
-#define DEFAULT_READ_TEMP_TIMER 10000
+#define DEFAULT_READ_TEMP_TIMER 60000
 #define DEFAULT_RECYCLE_DELAY 900000
-#define DEFAULT_HEAT_TIME 3600000
+#define DEFAULT_HEAT_TIME 2700000
 
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
@@ -35,18 +35,21 @@ long read_temp_delay = DEFAULT_READ_TEMP_TIMER;
 long recycle_delay = DEFAULT_RECYCLE_DELAY;
 long active_delay = DEFAULT_HEAT_TIME;
 
+myDelay occupancy_timer = myDelay(300000);          // Check occupancy every 5 minutes 300000
 myDelay sleep_temp_timer = myDelay(2000);           // Leave sensors running for 2 seconds 2000
-myDelay read_temp_timer = myDelay(read_temp_delay); // Check Temperatures every 10 seconds 10000
-myDelay heat_on_timer = myDelay(active_delay);      // Keep heat on for 1 hour 60*60*1000 = 3600000
+myDelay read_temp_timer = myDelay(read_temp_delay); // Check Temperatures every 1 minute 60000
+myDelay heat_on_timer = myDelay(active_delay);      // Keep heat on for 45 minutes 45*60*1000 = 2700000
 myDelay heat_reset_timer = myDelay(recycle_delay);  // Cycle heat off for 15 minutes 15*60*1000 = 900000
 
 float inside_temp, outside_temp;
 
 boolean system_on = false;
-boolean occupied = true;
+boolean occupied = false;
 boolean heat_on = false;
 boolean cold_out = false;
 boolean cold_in = false;
+
+boolean run_state_machine = true;
 
 void setup() {
   Serial.begin(115200);
@@ -90,9 +93,10 @@ void setup() {
   sleep_temp_timer.setRepeat(1);
   heat_on_timer.setRepeat(1);
   heat_reset_timer.setRepeat(1);
+  occupancy_timer.start();
   
   turn_system_off();
-  
+  read_load_cell();
   read_temps();
 }
 
@@ -136,6 +140,21 @@ void highlight_status(bool flag, char *tag) {
   display.print(" ");
 }
 
+void read_load_cell() {
+  /*
+   * Read cell
+   * if cell shows weight
+   *  if not occupied
+   *    set occupied
+   *    reduce temp timer to default
+   * else
+   *  if occupide
+   *    clear occupied
+   *    increast temp timer to 10 minutes
+   */
+  occupied = true;
+}
+
 void read_temps() {
   digitalWrite(LED_BUILTIN, HIGH);
 
@@ -172,17 +191,52 @@ void turn_off_heat() {
   heat_reset_timer.start();
 }
 
-void process_state_machine() {
+void process_temperature() {
   
-  if (outside_temp < 25.0) {
-    recycle_delay = 5*60*1000;
-    heat_reset_timer.setDelay(recycle_delay);
+  if (inside_temp < 25.0) {
+    if (!cold_in) {
+      cold_in = true;
+      active_delay = 55 * 60 * 1000;
+      heat_on_timer.setDelay(active_delay);
+      recycle_delay = 5*60*1000;
+      heat_reset_timer.setDelay(recycle_delay);
+    }
   } else {
-    recycle_delay = DEFAULT_RECYCLE_DELAY;
+    if (cold_in) {
+      active_delay = DEFAULT_HEAT_TIME;
+      heat_on_timer.setDelay(active_delay);
+      recycle_delay = DEFAULT_RECYCLE_DELAY;
+      heat_reset_timer.setDelay(recycle_delay);
+      cold_in = false;
+      cold_out = false;
+    }
   }
   
-  if (inside_temp < outside_temp) {
+  if (outside_temp < 35.0) {
+    if (!cold_out) {
+      cold_out = true;
+      active_delay = 50 * 60 * 1000;
+      heat_on_timer.setDelay(active_delay);
+      recycle_delay = 10*60*1000;
+      heat_reset_timer.setDelay(recycle_delay);
+    }
+  } else {
+    if (cold_out) {
+      active_delay = DEFAULT_HEAT_TIME;
+      heat_on_timer.setDelay(active_delay);
+      recycle_delay = DEFAULT_RECYCLE_DELAY;
+      heat_reset_timer.setDelay(recycle_delay);
+      cold_out = false;
+    }
+  }
+  
+  if (outside_temp <= 45.0) {
     if (!heat_on) {
+      active_delay = DEFAULT_HEAT_TIME;
+      heat_on_timer.setDelay(active_delay);
+      recycle_delay = DEFAULT_RECYCLE_DELAY;
+      heat_reset_timer.setDelay(recycle_delay);
+
       turn_on_heat();
     }
   } else {
@@ -194,12 +248,14 @@ void process_state_machine() {
 
 }
 
-void process_temperature() {
+void process_state_machine() {
   if (system_on) {
     if (outside_temp > 60.0) {
       turn_system_off();
     } else {
-      process_state_machine();
+      if (occupied) {
+        process_temperature();
+      }
     }
     
   } else {
@@ -211,11 +267,10 @@ void process_temperature() {
 
 void loop() {
 
-  if (read_temp_timer.update()) {
-    read_temps();
-    process_temperature();
+  if (occupancy_timer.update()) {
+    read_load_cell();
   }
-  
+
   if (sleep_temp_timer.update()) {
     sleep_probes();
   }
@@ -228,6 +283,16 @@ void loop() {
     turn_on_heat();
   }
 
+  if (read_temp_timer.update()) {
+    read_temps();
+    run_state_machine = true;
+  }
+
+  if (run_state_machine) {
+    process_state_machine();
+    run_state_machine = false;
+  }
+  
   display.clearDisplay();
   display.setCursor(0, 0);     // Start at top-left corner
 
