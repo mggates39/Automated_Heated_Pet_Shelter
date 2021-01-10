2c8aee6dd732e2aa61a239ea4a899cfbb85b549f
Marshall G Gates <mggates@gmail.com>
2021-01-09 23:57:39 -0500

    Add support for startup mode
    Read the temperatures every 10 seconds for 5 minutes to allow them to settle down on first start up.

--- Cat_House.ino
+++ Cat_House.ino
@@ -1,4 +1,3 @@
-
 /**************************************************************************/
 /*!
 This is based on a demo for the Adafruit MCP9808 breakout
@@ -34,8 +33,11 @@ Uses the MyDelay Arduino library at https://github.com/mggates39/MyDelay
 #define LOADCELL_SCK_PIN 3
 #define RELAY_PIN 8
 
+// Default read time 1 * 60 * 1000 is 1 minute or 60000 milliseconds
 #define DEFAULT_READ_TEMP_TIMER 60000
+// default heater off time 15 * 60 * 1000 is 15 minutes or 900000 milliseconds
 #define DEFAULT_RECYCLE_DELAY 900000
+// default heater on time 45 * 60 * 1000 is 45 minutes or 2700000 milliseconds
 #define DEFAULT_HEAT_TIME 2700000
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
@@ -55,19 +57,22 @@ void read_temps();
 void sleep_probes();
 void turn_off_heat();
 void turn_on_heat();
+void done_startup_mode();
 
 long read_temp_delay = DEFAULT_READ_TEMP_TIMER;
 long recycle_delay = DEFAULT_RECYCLE_DELAY;
 long active_delay = DEFAULT_HEAT_TIME;
 
-MyDelay occupancy_timer = MyDelay(300000, read_load_cell);          // Check occupancy every 5 minutes 300000ms
-MyDelay sleep_temp_timer = MyDelay(2000, sleep_probes, 1);          // Leave sensors running for 2 seconds 2000ms, only fire once
-MyDelay read_temp_timer = MyDelay(read_temp_delay, read_temps);     // Check Temperatures every 1 minute 60000ms
+MyDelay startup_timer = MyDelay(300000, done_startup_mode,1);       // We are in startup for 5 minutes 5*60*1000 = 300000ms, only fire once
+MyDelay occupancy_timer = MyDelay(300000, read_load_cell);          // Check occupancy every 5 minutes 5*60*1000 = 300000ms, repeat forever
+MyDelay sleep_temp_timer = MyDelay(2000, sleep_probes, 1);          // Leave sensors running for 2 seconds 2*1000 = 2000ms, only fire once
+MyDelay read_temp_timer = MyDelay(read_temp_delay, read_temps);     // Check Temperatures every 1 minute 60*1000 = 60000ms, repeat forever
 MyDelay heat_on_timer = MyDelay(active_delay, turn_off_heat, 1);    // Turn heat off after 45 minutes 45*60*1000 = 2700000ms, only fire once
 MyDelay heat_reset_timer = MyDelay(recycle_delay, turn_on_heat, 1); // Turn heat oon after 15 minutes 15*60*1000 = 900000ms, only fire once
 
 float inside_temp, outside_temp;
 
+boolean startup_mode = false;
 boolean system_on = false;
 boolean occupied = false;
 boolean heat_on = false;
@@ -101,12 +106,13 @@ void setup() {
 
   // Clear the buffer and display banner page
   display.clearDisplay();
-  display.setTextSize(2);      // Normal 1:1 pixel scale
+  display.setTextSize(2);      // Medium 2:1 pixel scale
   display.setTextColor(SSD1306_WHITE); // Draw white text
   display.setCursor(0, 0);     // Start at top-left corner
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
   display.println(F("Cat Palace\n  Heater"));
   display.display();
+  delay(1000); // Pause for 1 second
 
   // Set up for normal display
   display.clearDisplay();
@@ -137,8 +143,8 @@ void setup() {
 
   while(indoor_missing || outdoor_missing);
     
-  inside_sensor.setResolution(0); // sets the resolution mode of reading, the modes are defined in the table bellow:
-  outside_sensor.setResolution(0); // sets the resolution mode of reading, the modes are defined in the table bellow:
+  inside_sensor.setResolution(0); // sets the resolution mode of reading.
+  outside_sensor.setResolution(0); // sets the resolution mode of reading.
 
   occupancy_timer.start();
   
@@ -147,14 +153,20 @@ void setup() {
   read_load_cell();
   
   read_temps();
+
+  enter_startup_mode();
+}
+
+void set_read_delay( long milliseconds) {
+  read_temp_delay = milliseconds;
+  read_temp_timer.setDelay(read_temp_delay);
+  read_temp_timer.start();
 }
 
 void turn_system_off() {
   system_on = false;
 
-  read_temp_delay = 60 * 60 * 1000L;
-  read_temp_timer.setDelay(read_temp_delay);
-  read_temp_timer.start();
+  set_read_delay( 60 * 60 * 1000L); // 60 minutes
 
   heat_on_timer.stop();
   turn_off_heat();
@@ -163,14 +175,35 @@ void turn_system_off() {
 
 void turn_system_on() {
   system_on = true;
+  
+  if (startup_mode) {
+    startup_timer.stop();
+    done_startup_mode();
+  }
 
-  read_temp_delay = DEFAULT_READ_TEMP_TIMER;
-  read_temp_timer.setDelay(read_temp_delay);
-  read_temp_timer.start();
+  set_read_delay(DEFAULT_READ_TEMP_TIMER);
 
   heat_on_timer.stop();
   set_unoccupied_heat_cycle();
   turn_on_heat();
+}
+
+void enter_startup_mode() {
+//  Serial.println(F("Enter Startup"));
+  startup_mode = true;
+  
+  set_read_delay(10 * 1000L); // 10 seconds
+
+  startup_timer.start();
+}
+
+void done_startup_mode() {
+//  Serial.println(F("Leave Startup"));
+  startup_mode = false;
+
+  if (!system_on) {
+      turn_system_off();
+  }
 }
 
 void highlight_status(bool flag, const __FlashStringHelper *tag) {
@@ -196,6 +229,7 @@ void highlight_status(bool flag, char *tag) {
 }
 
 void read_load_cell() {
+//  Serial.println(F("Read Cell"));
   if (scale_found) {
     scale.power_up();
     float weight = scale.get_units(10);
@@ -203,16 +237,12 @@ void read_load_cell() {
     if (weight > 20.0) {
       if (!occupied) {
         occupied = true;
-        read_temp_delay = DEFAULT_READ_TEMP_TIMER;
-        read_temp_timer.setDelay(read_temp_delay);
-        read_temp_timer.start();
+        set_read_delay(DEFAULT_READ_TEMP_TIMER);
       }
     } else {
       if (occupied) {
         occupied = false;
-        read_temp_delay = 600000;
-        read_temp_timer.setDelay(read_temp_delay);
-        read_temp_timer.start();
+        set_read_delay(10 * 60 * 1000L); // 10 minutes
       }
     }
     scale.power_down();
@@ -221,6 +251,7 @@ void read_load_cell() {
 
 void read_temps() {
   digitalWrite(LED_BUILTIN, HIGH);
+//  Serial.println(F("Read Temp"));
 
   inside_sensor.wake();   // wake up, ready to read!
   outside_sensor.wake();   // wake up, ready to read!
@@ -335,7 +366,8 @@ void process_state_machine() {
 }
 
 void loop() {
-
+  startup_timer.update();
+  
   occupancy_timer.update();
 
   sleep_temp_timer.update();
